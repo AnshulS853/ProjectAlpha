fea3e78fc5e37110f62ae7495512bbde6a8e8336
AnshulS853 <singhanshul44@gmail.com>
2023-07-16 20:45:35 +0100

    Swift learning stage

--- ReceiptAlpha/ContentView.swift
+++ ReceiptAlpha/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var searchText = ""
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Project Alpha")
+                .fontWeight(.bold)
+                .multilineTextAlignment(.center)
+                .bold()
+                .font(.system(size:55))
+            
+            NavigationStack {
+            }
+            .padding(.top, -30.0)
+            .searchable(text: $searchText)
+            
+            Spacer()
+            
+            Image(systemName: "camera.fill")
+                .resizable()
+                .scaledToFit()
+                .padding(.bottom, -130.0)
+                .scaleEffect(0.3)
+                .foregroundColor(.purple)
+            Text("Scan a Receipt")
+                .multilineTextAlignment(.center)
         }
-        .padding()
+        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        .foregroundColor(/*@START_MENU_TOKEN@*/.gray/*@END_MENU_TOKEN@*/)
     }
 }
 
