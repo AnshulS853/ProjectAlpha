efa891348c3d25b1b700262e111b66fa6e834c2a
AnshulS853 <singhanshul44@gmail.com>
2023-07-16 14:29:55 +0100

    Initial Commit

---
+++ ReceiptAlpha/ReceiptAlphaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceiptAlphaApp.swift
+//  ReceiptAlpha
+//
+//  Created by Anshul Singh on 16/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ReceiptAlphaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
