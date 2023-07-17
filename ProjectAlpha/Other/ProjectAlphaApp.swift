4bcb104e9e3246726963e55484a96981ea227a84
AnshulS853 <singhanshul44@gmail.com>
2023-07-17 18:42:57 +0100

    Removed comments

--- ProjectAlpha/Other/ProjectAlphaApp.swift
+++ ProjectAlpha/Other/ProjectAlphaApp.swift
@@ -1,26 +1,3 @@
-//import SwiftUI
-//
-//@main
-//struct ProjectAlphaApp: App {
-//
-//    @AppStorage("isDarkMode") private var isDarkMode: Bool = {
-//        if let deviceDefault = UserDefaults.standard.object(forKey: "AppleInterfaceStyle") as? String,
-//           deviceDefault == "Dark" {
-//            return true
-//        } else {
-//            return false
-//        }
-//    }()
-//
-//    var body: some Scene {
-//        WindowGroup {
-//            MainView()
-//                .preferredColorScheme(isDarkMode ? .dark : .light)
-//        }
-//    }
-//}
-
-
 import SwiftUI
 
 @main
