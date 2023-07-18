1aac576efc5f16f2f5db02d69e45b10909039213
AnshulS853 <singhanshul44@gmail.com>
2023-07-19 00:12:13 +0100

    Updated comments

--- LockScreenWidget/LockScreenWidget.swift
+++ LockScreenWidget/LockScreenWidget.swift
@@ -1,6 +1,8 @@
 import WidgetKit
 import SwiftUI
 
+//Code for a dynamically updating widget using a timeline
+//This code needs to be updated as widget remains static
 struct Provider: TimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
         SimpleEntry(date: Date())
@@ -26,7 +28,7 @@ struct Provider: TimelineProvider {
         completion(timeline)
     }
 }
-
+//Code needs to be updated to reflect static widget
 struct SimpleEntry: TimelineEntry {
     let date: Date
 }
@@ -36,6 +38,7 @@ struct LockScreenWidgetEntryView : View {
     var entry: Provider.Entry
 
     var body: some View {
+        //UI for each widget
         switch widgetFamily {
         case .accessoryCircular:
             VStack{
@@ -69,7 +72,8 @@ struct LockScreenWidget: Widget {
 
         //Adding Widget Families
         .supportedFamilies([.accessoryCircular,.accessoryRectangular])
-
+        
+        //Text when placing the widget on lockscreen
         .configurationDisplayName("Project Alpha Widgets")
         .description("Quickly scan your receipts by\nplacing a widget on your lockscreen")
     }
@@ -77,6 +81,7 @@ struct LockScreenWidget: Widget {
 
 struct LockScreenWidget_Previews: PreviewProvider {
     static var previews: some View {
+        //Placed 2 preview windows to view single and double space lockscreen widget
         LockScreenWidgetEntryView(entry: SimpleEntry(date: Date()))
             .previewContext(WidgetPreviewContext(family: .accessoryCircular))
             .previewDisplayName("Circular")
