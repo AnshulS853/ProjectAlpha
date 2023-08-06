141c17b37888f93497c7be9d92a17638bb582d32
Ben Alexander <benjaminalexander747@gmail.com>
2023-08-06 13:09:50 +0100

    added more comments and a little window to display the scanned texts temporarily

--- ProjectAlpha/Views/MainView.swift
+++ ProjectAlpha/Views/MainView.swift
@@ -7,7 +7,8 @@ struct MainView: View {
 
     //Flag set to enable document scanner upon button press
     @State private var showDocumentScanner = false
-    @State private var texts: [ScanData] = []
+    // array of the texts (along with UUID) that have been returned from the text recognition
+    @State private var scannedTexts: [ScanData] = []
 
     var body: some View{
         NavigationView{
@@ -29,7 +30,20 @@ struct MainView: View {
                     }
                     Spacer()
                 }
-
+                // TEMPORARY code to show the scanned texts in a list, with each list item showing a 1 line preview of the text, which when clicked opens up the full text.
+                VStack() {
+                    List {
+                        ForEach(scannedTexts) { text in
+                            NavigationLink(destination: ScrollView {
+                                Text(text.content)
+                            }, label: {
+                                Text(text.content)
+                                    .lineLimit(1)
+                            })
+                        }
+                    }
+                }
+                
                 Spacer()
 
                 //Button directed to DocumentScannerView file
@@ -54,14 +68,20 @@ struct MainView: View {
             }
         }
     }
+    // this creates the DocumentScannerView, and once completed (document has been scanned) will format the text into an array for each document that was scanned
     func makeScannerView() -> DocumentScannerView {
+        // iterate over each document that was scanned
         let scanner = DocumentScannerView(completion: {
             textPerPage in
+            // if there is scanned text from the document scanner, then append to the scannedTexts array.
+            // format all the text from each page (this can be made its own function later on for readability)
             if let outputText = textPerPage?.joined(separator: "\n").trimmingCharacters(in: .whitespacesAndNewlines) {
+                // create a new data scan struct
                 let newScanData = ScanData(content: outputText)
-                self.texts.append(newScanData)
+                self.scannedTexts.append(newScanData)
             }
         })
+        // hide the document scanner
         self.showDocumentScanner = false
         return scanner
     }
